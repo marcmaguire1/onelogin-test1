ecb1085bb4f9aea37c6a2f33099c7ae0cda29d9f
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-07-26 11:10:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,6 +50,16 @@ variable "user1_email" {
   default = "john.smith@johnsmithmail.com"
 }
 
+variable "user1_firstname" {
+  type = string
+  default = "john"
+}
+
+variable "user1_lastname" {
+  type = string
+  default = "smith"
+}
+
 variable "user2_username" {
   type = string
   default = "joan.smith"
@@ -60,6 +70,15 @@ variable "user2_email" {
   default = "joan.smith@joansmithmail.com"
 }
 
+variable "user2_firstname" {
+  type = string
+  default = "joan"
+}
+
+variable "user2_lastname" {
+  type = string
+  default = "smith"
+}
 ############ USERS ################
 
 #### Users via generic REST Provider
@@ -67,17 +86,17 @@ variable "user2_email" {
 ## example of how to create a new user in your OneLogin environment using the V2 Users endpoint
 resource "restapi_object" "oneloginuser1" {
   path = "/api/2/users"
-  data = "{ \"email\": \"${var.user2_email}\", \"username\": \"${var.user2_username}\", \"firstname\": \"Davennnnnn\", \"lastname\": \"Boe\"}"
+  data = "{ \"email\": \"${var.user2_email}\", \"username\": \"${var.user2_username}\", \"firstname\": \${var.user2_firstname}\", \"lastname\": \"${var.user2_lastname}\"}"
 }
 
 #### Users via OneLogin Provider
 
-# create some standard user for testing
+# create some standard user for testing. Make sure you have created a "custom user field" called "food" in your target OneLogin envionrment for this example to work 
 resource onelogin_users username {
   username = var.user1_username
   email    = var.user1_email
-  firstname = "tim"
-  lastname = "test"
+  firstname = var.user1_firstname
+  lastname = var.user1_lastname
   status = 1
   state = 1
   custom_attributes = {food = "pizza"}
