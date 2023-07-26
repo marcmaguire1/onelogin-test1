8727c9322e16a1af8f197be80790c99d6df1a242
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-07-26 09:56:44 +0100

    Update main.tf
    added users examples

--- main.tf
+++ main.tf
@@ -29,3 +29,26 @@ provider "onelogin" {
 client_id = var.client_id
 client_secret = var.client_secret
 }
+
+############ USERS ################
+
+#### Users via generic REST Provider
+
+## example of how to create a new user in your OneLogin environment using the V2 Users endpoint
+resource "restapi_object" "oneloginuser1" {
+  path = "/api/2/users"
+  data = "{ \"email\": \"${var.da_user2_email}\", \"username\": \"${var.da_user2_username}\", \"firstname\": \"Davennnnnn\", \"lastname\": \"Boe\"}"
+}
+
+#### Users via OneLogin Provider
+
+# create some standard user for testing
+resource onelogin_users username {
+  username = var.da_user1_username
+  email    = var.da_user1_email
+  firstname = "tim"
+  lastname = "test"
+  status = 1
+  state = 1
+  custom_attributes = {food = "pizza"}
+}
