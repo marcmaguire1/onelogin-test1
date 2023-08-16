d088482fdf51cb376b095621d9f8c847587e2311
marcmaguire1 <109895334+marcmaguire1@users.noreply.github.com>
2023-08-16 12:31:16 +0200

    Update smart_hook_pa.tf

--- smart_hook_pa.tf
+++ smart_hook_pa.tf
@@ -1,1 +1,55 @@
-tbc
+terraform {
+  required_providers {
+    restapi = {
+      source = "Mastercard/restapi"
+      version = "1.18.0"
+    }
+    onelogin = {
+      source  = "onelogin/onelogin"
+      version = "0.2.0"
+    }
+  }
+} 
+
+provider "restapi" {
+  # Configuration options
+   uri                  = "https://api.us.onelogin.com/"
+   write_returns_object = true
+   oauth_client_credentials {
+      oauth_client_id = var.ol_client_id
+      oauth_client_secret = var.ol_client_secret
+      oauth_token_endpoint = "https://api.us.onelogin.com/auth/oauth2/v2/token"
+  }
+}
+
+provider "onelogin" {
+  # Configuration options
+client_id = var.ol_client_id
+client_secret = var.ol_client_secret
+}
+
+########## VARS ###########
+
+variable "ol_client_id" {
+  type = string
+  description = "Client ID for API Credential created in target OneLogin env"
+}
+
+variable "ol_client_secret" {
+  type = string
+  description = "Client Secret for API Credential created in target OneLogin env"
+}
+
+############ Smart Hook env vars ################
+
+## example of how to create some env vars for Smart Hooks to use
+resource "restapi_object" "oneloginsmarthook_vars" {
+  path = "/api/2/hooks/envs"
+  data = "{ \"name\": \"google_pol_id\", \"value\": \"351165\"}"
+}
+
+############ Smart Hook ################
+
+#### create smart hook via generic REST Provider
+
+## example of how to create a new user in your OneLogin environment using the V2 Users endpoint
